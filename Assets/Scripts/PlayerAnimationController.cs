18746978dadce333cbee713cdede8dabd46ca149
uberbax <thwbbb@gmail.com>
2016-06-26 23:59:26 +0300

    F! added character rig and sample animations for him, also added character avatar (render texture)

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	
+	void Update () 
+    {
+        if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+	}
+}
