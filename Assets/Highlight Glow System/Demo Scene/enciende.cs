3378b38bb9d4c0e0f612542b16d0de442e640e43
uberbax <thwbbb@gmail.com>
2016-06-27 16:52:33 +0300

    F! added highlighting system

---
+++ Assets/Highlight Glow System/Demo Scene/enciende.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enciende : MonoBehaviour {
+
+	public GameObject elCubo;
+	private bool isOn=false;
+	public GameObject DirectionalLight;
+	private bool DirLightIsOn=true;
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (20, 20, 150, 60), "Crate On/Off")) {
+			if (!isOn) {
+				shaderGlow gls= elCubo.GetComponent<shaderGlow>();
+				gls.lightOn();
+				isOn=true;
+			}
+			else {
+				shaderGlow gls= elCubo.GetComponent<shaderGlow>();
+				gls.lightOff();
+				isOn=false;
+			}
+		}
+
+		if (GUI.Button (new Rect (20, 90, 150, 60), "Light On/Off")) {
+			if (!DirLightIsOn) {
+				DirectionalLight.SetActive(true);
+				DirLightIsOn=true;
+			}
+			else {
+				shaderGlow gls= elCubo.GetComponent<shaderGlow>();
+				gls.lightOff();
+				DirectionalLight.SetActive(false);
+				DirLightIsOn=false;
+			}
+		}
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
